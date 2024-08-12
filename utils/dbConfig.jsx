4f00041133554f518982e6b90681a98177f8dcda
Ayush chauhan <113831410+AyushChauhan9389@users.noreply.github.com>
2024-08-12 18:00:03 +0000

    Fix

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  process.env.DATABASE_URL
+  "postgresql://data_owner:LdVZ8QI7vyCx@ep-black-star-a17bfhy8.ap-southeast-1.aws.neon.tech/data?sslmode=require"
 );
 export const db = drizzle(sql, { schema });
